fad2f76255cca58cf31376037cdfba4143cd9680
chriscortes4u <chriscortes4u@yahoo.com>
2016-11-07 13:01:06 -0500

    adding commits for halfway there

--- data-field.js
+++ data-field.js
@@ -1,74 +1,76 @@
-const dalNoSQL= require('./DAL/no-sql.js');
+const dalNoSQL = require('./DAL/no-sql.js');
 
-const personData = [{
-    firstName: "Joe",
-    lastName: "neighbor",
-    class: "touring",
-    maker: "Honda",
-    year: "2003"
-  },{
-    firstName: "Matt",
-    lastName: "Rightside",
-    class: "cruiser",
-    maker: "Harley Davidson",
-    year: "2010"
-  },{
-    firstName: "Josh",
-    lastName: "Farside",
-    class: "scooter",
-    maker: "Honda",
-    year: "2015"
-  },{
-    firstName: "Kendra",
-    lastName: "Kitkat",
-    class: "scooter",
-    maker: "Vespa",
-    year: "2011"
-  },{
-    firstName: "Cameron",
-    lastName: "KillaCam",
-    class: "cruiser",
-    maker: "Harley Davidson",
-    year: "2008"
-  },{
-    firstName: "Jared",
-    lastName: "frontside",
-    class: "sportbike",
-    maker: "suzuki",
-    year: "2014"
-  },{
-    firstName: "Derek",
-    lastName: "Guruada",
-    class: "scooter",
-    maker: "Vespa",
-    year: "2010"
-  },{
-    firstName: "Andrea",
-    lastName: "Ohio",
-    class: "sportbike",
-    maker: "suzuki",
-    year: "2016"
-  },{
-    firstName: "Alex",
-    lastName: "Longhair",
-    class: "touring",
-    maker: "Honda",
-    year: "2009"
-  },{
-    firstName: "Chris",
-    lastName: "Cortes",
-    class: "sportbike",
-    maker: "suzuki",
-    year: "2016"
-}];
+// const personData = [{
+//     firstName: "Joe",
+//     lastName: "neighbor",
+//     class: "touring",
+//     maker: "Honda",
+//     year: "2003"
+//   },{
+//     firstName: "Matt",
+//     lastName: "Rightside",
+//     class: "cruiser",
+//     maker: "Harley Davidson",
+//     year: "2010"
+//   },{
+//     firstName: "Josh",
+//     lastName: "Farside",
+//     class: "scooter",
+//     maker: "Honda",
+//     year: "2015"
+//   },{
+//     firstName: "Kendra",
+//     lastName: "Kitkat",
+//     class: "scooter",
+//     maker: "Vespa",
+//     year: "2011"
+//   },{
+//     firstName: "Cameron",
+//     lastName: "KillaCam",
+//     class: "cruiser",
+//     maker: "Harley Davidson",
+//     year: "2008"
+//   },{
+//     firstName: "Jared",
+//     lastName: "frontside",
+//     class: "sportbike",
+//     maker: "suzuki",
+//     year: "2014"
+//   },{
+//     firstName: "Derek",
+//     lastName: "Guruada",
+//     class: "scooter",
+//     maker: "Vespa",
+//     year: "2010"
+//   },{
+//     firstName: "Andrea",
+//     lastName: "Ohio",
+//     class: "sportbike",
+//     maker: "suzuki",
+//     year: "2016"
+//   },{
+//     firstName: "Alex",
+//     lastName: "Longhair",
+//     class: "touring",
+//     maker: "Honda",
+//     year: "2009"
+//   },{
+//     firstName: "Chris",
+//     lastName: "Cortes",
+//     class: "sportbike",
+//     maker: "suzuki",
+//     year: "2016"
+// }];
 
-const classData =[{
-  class: "touring",
-  class: "sportbike",
-  class: "scooter",
-  class: "cruiser"
-}]
-
+// const classData = [
+//     {
+//         class: "touring",
+//         class: "sportbike",
+//         class: "scooter",
+//         class: "cruiser"
+//     }
+// ]
+//
 const makerData = [{
   maker: "suzuki",
   maker: "Honda",
@@ -76,22 +78,22 @@ const makerData = [{
   maker: "Harley Davidson"
 }]
 
-
-function callback (msgHeader) {
-  return function (err, response) {
-    if (err) return console.log('ERROR:\n', err.message)
-    return console.log(msgHeader, response)
-  }
+function callback(msgHeader) {
+    return function(err, response) {
+        if (err)
+            return console.log('ERROR:\n', err.message)
+        return console.log(msgHeader, response)
+    }
 }
 
-personData.forEach(function(person) {
-  dalNoSQL.createPerson(person, callback('PERSON CREATED:\n'))
-});
-
-classData.forEach(function(class) {
-  dalNoSQL.createClass(class, callback('CLASS CREATED:\n'))
-});
+// personData.forEach(function(person) {
+//   dalNoSQL.createPerson(person, callback('PERSON CREATED:\n'))
+// });
+//
+// classData.forEach(function(class) {
+//     dalNoSQL.createClass(class, callback('CLASS CREATED:\n'))
+// });
 
 makerData.forEach(function(maker) {
-  dalNoSQL.createMaker(maker, callback('Maker CREATED:\n'))
+    dalNoSQL.createMaker(maker, callback('Maker CREATED:\n'))
 });
